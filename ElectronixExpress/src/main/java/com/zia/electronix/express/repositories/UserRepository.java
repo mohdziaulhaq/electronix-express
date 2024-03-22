6507a2ad3230c53415072a8077ab0b625b6d5173
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-22 04:22:59 -0500

    added User Repository

--- ElectronixExpress/src/main/java/com/zia/electronix/express/repositories/UserRepository.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/repositories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.zia.electronix.express.repositories;public class UserRepository {
+package com.zia.electronix.express.repositories;
+
+import com.zia.electronix.express.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
 }

06dd9ef932db492b3a20a109fa62f2133f8226e4
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-22 03:01:38 -0500

    added spring initilzr project

---
+++ ElectronixExpress/src/test/java/com/zia/electronix/express/ElectronixExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zia.electronix.express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronixExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

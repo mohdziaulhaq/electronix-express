aa092a3c7567eb7b7e6ac6cfc3b17afb0c24c0e1
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-22 05:00:17 -0500

    added user service and its implementation

--- ElectronixExpress/src/main/java/com/zia/electronix/express/services/UserService.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/services/UserService.java
@@ -1,2 +1,32 @@
-package com.zia.electronix.express.services;public class UserService {
+package com.zia.electronix.express.services;
+
+import com.zia.electronix.express.dtos.UserDto;
+import com.zia.electronix.express.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface UserService {
+
+    //create
+    UserDto createUser(UserDto userDto);
+
+    //update
+    UserDto updateUser(UserDto userDto, String userId);
+
+    //delete
+    void deleteUser(String userId);
+
+    //get all users
+    List<UserDto> getAllUsers();
+
+    //get single user by id
+    UserDto getUser(String userId);
+
+    //get single user by email
+    UserDto getUserByEmail(String email);
+
+    //search user
+    List<UserDto> searchUser(String keyword);
+
 }
--- ElectronixExpress/src/main/java/com/zia/electronix/express/services/impl/UserServiceImpl.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/services/impl/UserServiceImpl.java
@@ -1,2 +1,74 @@
-package com.zia.electronix.express.services.impl;public class UserServiceImpl {
+package com.zia.electronix.express.services.impl;
+
+import com.zia.electronix.express.dtos.UserDto;
+import com.zia.electronix.express.entities.User;
+import com.zia.electronix.express.services.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+
+        String userId = UUID.randomUUID().toString();
+
+        //dto to entity
+        User user = dtoToEntity(userDto);
+
+        //entity to dto
+        UserDto returnDto = entityToDto(user);
+
+        return returnDto;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDto getUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> searchUser(String keyword) {
+        return null;
+    }
+
+    User dtoToEntity(UserDto dto){
+        User user = User.builder().userId(dto.getUserId()).
+                name(dto.getName()).
+                password(dto.getPassword()).
+                email(dto.getEmail()).
+                gender(dto.getGender()).build();
+        return user;
+
+    }
+
+    UserDto entityToDto(User savedUser){
+        UserDto userDto = UserDto.builder().userId(savedUser.getUserId()).
+                name(savedUser.getName()).
+                password(savedUser.getPassword()).
+                email(savedUser.getEmail()).
+                gender(savedUser.getGender()).build();
+        return userDto;
+
+    }
 }

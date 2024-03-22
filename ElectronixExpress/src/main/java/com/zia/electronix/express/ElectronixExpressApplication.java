0e7a331d147e4a6ac19afad4a6b0cad82bbdd02b
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-22 04:24:17 -0500

    updated code

--- ElectronixExpress/src/main/java/com/zia/electronix/express/ElectronixExpressApplication.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/ElectronixExpressApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElectronixExpressApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElectronixExpressApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronixExpressApplication.class, args);
+    }
 
 }
--- ElectronixExpress/src/main/java/com/zia/electronix/express/controller/HomeController.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.zia.electronix.express.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
 public class HomeController {
+
+    @GetMapping
+    public String homecontroller(){
+        return "Welcome to Electronix Express Home Page";
+    }
 }

6e0167fbfa09d24ed4c1a9a494bc6f30ab601bfa
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-22 04:22:40 -0500

    added User Entity

--- ElectronixExpress/src/main/java/com/zia/electronix/express/entities/User.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/entities/User.java
@@ -1,2 +1,28 @@
-package com.zia.electronix.express.entities;public class User {
+package com.zia.electronix.express.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+
+    @Column(name = "user_password", length = 10)
+    private String password;
+
+    private String gender;
 }

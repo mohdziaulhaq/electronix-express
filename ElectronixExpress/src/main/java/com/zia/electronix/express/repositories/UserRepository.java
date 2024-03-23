821da144191d03efa664fede263b6bea04fc390e
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-23 00:54:21 -0500

    added custom query finder methods in repository

--- ElectronixExpress/src/main/java/com/zia/electronix/express/repositories/UserRepository.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/repositories/UserRepository.java
@@ -3,8 +3,13 @@ package com.zia.electronix.express.repositories;
 import com.zia.electronix.express.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+    List<User> findByNameContaining(String keywords);
 
 }

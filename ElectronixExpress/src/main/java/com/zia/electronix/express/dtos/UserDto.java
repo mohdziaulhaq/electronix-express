510cb6af0d531cec3100de5bccd5a7f2bff79c5b
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-24 01:22:21 -0500

    added validators in user dto

--- ElectronixExpress/src/main/java/com/zia/electronix/express/dtos/UserDto.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/dtos/UserDto.java
@@ -1,5 +1,9 @@
 package com.zia.electronix.express.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,11 +14,16 @@ import lombok.*;
 public class UserDto {
     private String userId;
 
+    @Size(min=3, max = 15, message= "Invalid name")
     private String name;
 
+//    @Email(message = "Invalid email")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",message = "Invalid email")
     private String email;
 
+    @NotBlank(message = "Password is required")
     private String password;
 
+    @Size(min = 4,max = 6, message = "Invalid gender")
     private String gender;
 }

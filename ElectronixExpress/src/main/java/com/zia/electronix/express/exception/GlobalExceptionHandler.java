9092130f69b9636bec73e0f11d7e23836a7c2ac4
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-03-26 00:51:13 -0500

    GlobalExceptionHandler.java created

--- ElectronixExpress/src/main/java/com/zia/electronix/express/exception/GlobalExceptionHandler.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/exception/GlobalExceptionHandler.java
@@ -1,2 +1,43 @@
-package com.zia.electronix.express.exception;public class GlobalExceptionHandler {
+package com.zia.electronix.express.exception;
+
+import com.zia.electronix.express.dtos.ApiResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponseMessage> resourceNotFoundException(ResourceNotFoundException ex){
+        ApiResponseMessage message = ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.NOT_FOUND).build();
+        logger.info("Exception Handler invoked!!");
+        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        List<ObjectError> allErrors =  ex.getBindingResult().getAllErrors();
+        Map<String,Object> response = new HashMap<>();
+        allErrors.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String name = ((FieldError) objectError).getField();
+            response.put(name,message);
+        });
+
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
 }

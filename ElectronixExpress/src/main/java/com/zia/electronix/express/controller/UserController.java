42a3698d74ceca6b858b00fdaf7c612d9f78bc38
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-01-17 05:55:07 -0600

    file serve to user code completed

--- ElectronixExpress/src/main/java/com/zia/electronix/express/controller/UserController.java
+++ ElectronixExpress/src/main/java/com/zia/electronix/express/controller/UserController.java
@@ -6,16 +6,22 @@ import com.zia.electronix.express.dtos.PageableResponse;
 import com.zia.electronix.express.dtos.UserDto;
 import com.zia.electronix.express.services.FileService;
 import com.zia.electronix.express.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -30,6 +36,8 @@ public class UserController {
 
     @Value("${user.profile.image-path}")
     private String imageUploadPath;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     //create
     @PostMapping
@@ -100,6 +108,17 @@ public class UserController {
     }
 
     //serve user image
+
+    @GetMapping("/image/{userId}")
+    public void serveUserImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+
+        UserDto user = userService.getUserById(userId);
+        logger.info("User image name:{}", user.getImageName());
+
+        InputStream resource = fileService.getResource(imageUploadPath,user.getImageName());
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
+    }
 
 
 
